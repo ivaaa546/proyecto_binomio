9e26448327a5596c4b3ae07960accf1cff10e26d
ivaaa546 <ivanii5862@gmail.com>
2023-09-27 17:58:07 -0600

    Le coloque binomio sin expandir y puse bucles while a las case para que el usario si se equivoca pueda a volver a escribir

--- pro.cpp
+++ pro.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <cmath>
 #include <cctype>
 using namespace std;
@@ -10,7 +11,6 @@ struct literales
     int  exponente_literal1;
     int  exponente_literal2;
 };
-
 
 //  para calcular el factorial de un número
 unsigned long long Factorial(int num) {
@@ -29,6 +29,18 @@ unsigned long long Combinacion(int n, int k) {
     
     return Factorial(n) / (Factorial(n - k) * Factorial(k));
 }
+//resta par el binomio sin expandir
+unsigned long long resta(int n1, int n2){
+
+    return n1-n2;
+
+}
+//sin expansion
+void sin_expansion(int exponente, int coeficiente1, int coeficiente2, char l1, char l2, int expl1, int expl2, int no_econtrar) {
+    cout << "(" << coeficiente1 << l1 << "^" << expl1 << " + " << coeficiente2 << l2 << "^" << expl2 << ")^" << exponente << endl;
+}
+
+
 
 //expansión del binomio //
 void expansion_bionomio(int combinacion_, int coeficiente1, int coeficiente2, char l1, char l2, int expl1, int expl2) {
@@ -45,12 +57,12 @@ void expansion_bionomio(int combinacion_, int coeficiente1, int coeficiente2, ch
     } 
 }
 
-//calcular resultado//
-void calcularResultado(int exponente, int coeficiente1_, int coeficiente2, literales exp) {
+//calcular resultado bionomio expandido//
+void calcularResultado(int calcular_exponente, int calcular_coeficiente1_, int calcular_coeficiente2, literales exp) {
     cout << "\nResultado: ";
-    for (int i = 0; i <= exponente; ++i) {
-        unsigned long long calculo_combinacion = Combinacion(exponente, i);
-        int resultado = calculo_combinacion * pow(coeficiente1_, exponente - i) * pow(coeficiente2, i);
+    for (int i = 0; i <= calcular_exponente; ++i) {
+        unsigned long long calculo_combinacion = Combinacion(calcular_exponente, i);
+        int resultado = calculo_combinacion * pow(calcular_coeficiente1_, calcular_exponente - i) * pow(calcular_coeficiente2, i);
 
         // Esta parte evita imprimir términos con coeficientes iguales a cero
         if (resultado != 0) {
@@ -60,10 +72,10 @@ void calcularResultado(int exponente, int coeficiente1_, int coeficiente2, liter
             cout << resultado;
 
             // Literal 1 
-            if (exp.exponente_literal1 * (exponente - i) > 0) {
+            if (exp.exponente_literal1 * (calcular_exponente - i) > 0) {
                 cout << exp.literal1;
-                if (exp.exponente_literal1 * (exponente - i) > 1) {
-                    cout << "^" << exp.exponente_literal1 * (exponente - i);
+                if (exp.exponente_literal1 * (calcular_exponente - i) > 1) {
+                    cout << "^" << exp.exponente_literal1 * (calcular_exponente - i);
                 }
             }
 
@@ -79,35 +91,72 @@ void calcularResultado(int exponente, int coeficiente1_, int coeficiente2, liter
     }
     cout << endl;
 }
+//solucion sin expansion
+void resultado_sin(int calcular_exponente, int calcular_coeficiente1_, int calcular_coeficiente2, int num_aecontrar, literales exp) {
+    unsigned long long calcular_combinacion = Combinacion(calcular_exponente, num_aecontrar);
+    unsigned long long resta = calcular_exponente - num_aecontrar;
+    unsigned long long exponenteY = calcular_exponente;
+    unsigned long long literal1 =  exp.exponente_literal1 * resta;
+    unsigned long long literal2 = exp.exponente_literal2 * num_aecontrar;
+    unsigned long long resultado = calcular_combinacion * pow(calcular_coeficiente1_, resta) * pow(calcular_coeficiente2, exponenteY);
+    
+    cout << "El resultado sin expandir es: " << resultado<<exp.literal1<<"^"<<literal1<<" "<<exp.literal2<<"^"<<literal2<<endl;
+}
 
 int main() {
+    char opciones;
     int exponente;
     int coeficiente1_, coeficiente2_;
+    int num_aecontrar;
     literales exp;
 
-    cout << "Ingresa el exponente del binomio: ";
-    cin >> exponente;
+    cout << "Ingrese una opcion:\n 1.Binomio Expandido\n 2.Binomio sin expandir "<<endl;
+    cin>> opciones;
+
+
+        switch (opciones)
+    {
+    //Opcion 1.Binomio Expandido
+    case '1':
+
+    while (true) {
+        cout << "Ingresa el exponente del binomio: ";
+        if (cin >> exponente) {
+            break; // Salir del bucle si se ingresó un número válido
+        } else {
+            cout << "No es un numero. Intente de nuevo." << endl;
+            cin.clear(); // Restablecer el estado de cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer de entrada
+        }
+    }
+
     
-    if (cin.fail()) {
-    cout << "No es un número. Intente de nuevo." << endl;
-    return 1;
-}
-    cout << "\nIngrese el primer coeficiente: ";
-    cin >> coeficiente1_;
-    if (cin.fail()) {
-    cout << "No es un digito, por favor intente de nuevo" << endl;
-    return 1;
-}
-    cout << "Ingrese el primer literal del coeficiente: ";
+    while (true) {
+        cout << "\nIngrese el primer coeficiente: ";
+        if (cin >> coeficiente1_) {
+            break; // Salir del bucle si se ingresó un número válido
+        } else {
+            cout << "No es un numero. Intente de nuevo." << endl;
+            cin.clear(); // Restablecer el estado de cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer de entrada
+        }
+    }
+
+    cout << "Ingrese el primer literal del coeficiente y su exponente (por ejemplo : x 2): ";
     cin >> exp.literal1>>exp.exponente_literal1;
     
-    cout << "\nIngrese el segundo coeficiente: ";
-    cin >> coeficiente2_;
-    if (cin.fail()) {
-    cout << "No es un digito, por favor intente de nuevo" << endl;
-    return 1;
-}
-    cout << "Ingrese el segundo literal del coeficiente: ";
+    
+     while (true) {
+        cout << "\nIngrese el segundo coeficiente: ";
+        if (cin >> coeficiente2_) {
+            break; // Salir del bucle si se ingresó un número válido
+        } else {
+            cout << "No es un numero. Intente de nuevo." << endl;
+            cin.clear(); // Restablecer el estado de cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer de entrada
+        }
+    }
+    cout << "Ingrese el segundo literal del coeficiente y su exponente (por ejemplo: y 2): ";
     cin >> exp.literal2>>exp.exponente_literal2; 
 
     cout << "(" << coeficiente1_ << exp.literal1 <<"^"<<exp.exponente_literal1<<" + " << coeficiente2_ << exp.literal2<<"^"<<exp.exponente_literal2<< ")^" << exponente<< endl;
@@ -117,6 +166,62 @@ int main() {
     // Llamada a la función para calcular y mostrar el resultado
     calcularResultado(exponente, coeficiente1_, coeficiente2_, exp);
     cout << endl;
+        break;
+    //Binomio sin expandir
+    case '2': 
+   
+    while (true) {
+        cout << "Ingresa el exponente del binomio: ";
+        if (cin >> exponente) {
+            break; // Salir del bucle si se ingresó un número válido
+        } else {
+            cout << "No es un numero. Intente de nuevo." << endl;
+            cin.clear(); // Restablecer el estado de cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer de entrada
+        }
+    }
 
-    return 0;
+        while (true) {
+        cout << "\nIngrese el primer coeficiente: ";
+        if (cin >> coeficiente1_) {
+            break; // Salir del bucle si se ingresó un número válido
+        } else {
+            cout << "No es un numero. Intente de nuevo." << endl;
+            cin.clear(); // Restablecer el estado de cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer de entrada
+        }
+    }
+    cout << "Ingrese el primer literal del coeficiente y su exponente (por ejemplo: x 2): ";
+    cin >> exp.literal1 >> exp.exponente_literal1;
+    
+    
+    while (true) {
+        cout << "\nIngrese el segundo coeficiente: ";
+        if (cin >> coeficiente2_) {
+            break; // Salir del bucle si se ingresó un número válido
+        } else {
+            cout << "No es un numero. Intente de nuevo." << endl;
+            cin.clear(); // Restablecer el estado de cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer de entrada
+        }
+    }
+
+    cout << "Ingrese el segundo literal del coeficiente y su exponente (por ejemplo: y 2): ";
+    cin >> exp.literal2 >> exp.exponente_literal2; 
+
+    //Llamada para ver la operacion
+    sin_expansion(exponente, coeficiente1_, coeficiente2_, exp.literal1, exp.literal2, exp.exponente_literal1, exp.exponente_literal2, num_aecontrar);
+    
+
+    cout << "Ingrese el digito que desea encontrar (para encontrar un numero se toma el 0 como 1 y el 1 como 2 asi sucesivamente): ";
+    cin >> num_aecontrar;
+   // Para mostrar como se hara la operacion
+    unsigned long long restaresultado = resta(exponente,num_aecontrar);
+    cout <<exponente<<"C"<<num_aecontrar<<" "<<"(" << coeficiente1_ << exp.literal1 <<"^"<<exp.exponente_literal1<<")"<<restaresultado<<"  " << coeficiente2_ << exp.literal2<<"^"<<exp.exponente_literal2<< ")^" << num_aecontrar<< endl;
+
+    // Llamada a la función para calcular y mostrar el resultado
+    resultado_sin(exponente, coeficiente1_, coeficiente2_, num_aecontrar, exp);
+    break;
+}
+return 0;
 }
--- prueba.cpp
+++ prueba.cpp
@@ -11,7 +11,6 @@ struct literales
     int  exponente_literal2;
 };
 
-
 //  para calcular el factorial de un número
 unsigned long long Factorial(int num) {
     unsigned long long calculo_factorial = 1;
@@ -29,6 +28,18 @@ unsigned long long Combinacion(int n, int k) {
     
     return Factorial(n) / (Factorial(n - k) * Factorial(k));
 }
+//resta par el binomio sin expandir
+unsigned long long resta(int n1, int n2){
+
+    return n1-n2;
+
+}
+//sin expansion
+void sin_expansion(int exponente, int coeficiente1, int coeficiente2, char l1, char l2, int expl1, int expl2, int no_econtrar) {
+    cout << "(" << coeficiente1 << l1 << "^" << expl1 << " + " << coeficiente2 << l2 << "^" << expl2 << ")^" << exponente << endl;
+}
+
+
 
 //expansión del binomio //
 void expansion_bionomio(int combinacion_, int coeficiente1, int coeficiente2, char l1, char l2, int expl1, int expl2) {
@@ -45,12 +56,12 @@ void expansion_bionomio(int combinacion_, int coeficiente1, int coeficiente2, ch
     } 
 }
 
-//calcular resultado//
-void calcularResultado(int exponente, int coeficiente1_, int coeficiente2, literales exp) {
+//calcular resultado bionomio expandido//
+void calcularResultado(int calcular_exponente, int calcular_coeficiente1_, int calcular_coeficiente2, literales exp) {
     cout << "\nResultado: ";
-    for (int i = 0; i <= exponente; ++i) {
-        unsigned long long calculo_combinacion = Combinacion(exponente, i);
-        int resultado = calculo_combinacion * pow(coeficiente1_, exponente - i) * pow(coeficiente2, i);
+    for (int i = 0; i <= calcular_exponente; ++i) {
+        unsigned long long calculo_combinacion = Combinacion(calcular_exponente, i);
+        int resultado = calculo_combinacion * pow(calcular_coeficiente1_, calcular_exponente - i) * pow(calcular_coeficiente2, i);
 
         // Esta parte evita imprimir términos con coeficientes iguales a cero
         if (resultado != 0) {
@@ -60,10 +71,10 @@ void calcularResultado(int exponente, int coeficiente1_, int coeficiente2, liter
             cout << resultado;
 
             // Literal 1 
-            if (exp.exponente_literal1 * (exponente - i) > 0) {
+            if (exp.exponente_literal1 * (calcular_exponente - i) > 0) {
                 cout << exp.literal1;
-                if (exp.exponente_literal1 * (exponente - i) > 1) {
-                    cout << "^" << exp.exponente_literal1 * (exponente - i);
+                if (exp.exponente_literal1 * (calcular_exponente - i) > 1) {
+                    cout << "^" << exp.exponente_literal1 * (calcular_exponente - i);
                 }
             }
 
@@ -79,30 +90,45 @@ void calcularResultado(int exponente, int coeficiente1_, int coeficiente2, liter
     }
     cout << endl;
 }
-
-int main() {
-    int exponente;
-    int coeficiente1_, coeficiente2_;
-    literales exp;
-
-    cout << "Ingresa el exponente del binomio: ";
-    cin >> exponente;
+//solucion sin expansion
+void resultado_sin(int calcular_exponente, int calcular_coeficiente1_, int calcular_coeficiente2, int num_aecontrar, literales exp) {
+    unsigned long long calcular_combinacion = Combinacion(calcular_exponente, num_aecontrar);
+    unsigned long long resta = calcular_exponente - num_aecontrar;
+    unsigned long long exponenteY = calcular_exponente;
+    unsigned long long literal1 =  exp.exponente_literal1 * resta;
+    unsigned long long literal2 = exp.exponente_literal2 * num_aecontrar;
+    unsigned long long resultado = calcular_combinacion * pow(calcular_coeficiente1_, resta) * pow(calcular_coeficiente2, exponenteY);
     
-    if (cin.fail()) {
-    cout << "No es un número. Intente de nuevo." << endl;
-    return 1;
+    cout << "El resultado sin expandir es: " << resultado<<exp.literal1<<"^"<<literal1<<" "<<exp.literal2<<"^"<<literal2<<endl;
 }
 
+int main() {
+    char opciones;
+    int exponente;
+    int coeficiente1_, coeficiente2_;
+    int num_aecontrar;
+    literales exp;
 
-    cout << "\nIngrese el primer coeficiente: ";
+    cout << "Ingrese una opcion:\n 1.Binomio Expandido\n 2.Binomio sin expandir "<<endl;
+    cin>> opciones;
+
+
+        switch (opciones)
+    {
+    case '1':
+    cout << "Ingresa el exponente del binomio: ";
+    cin >> exponente;
+    if (cin.fail()) {
+    cout << "No es un numero. Intente de nuevo." << endl;
+    return 1;
+}
+        cout << "\nIngrese el primer coeficiente: ";
     cin >> coeficiente1_;
     if (cin.fail()) {
     cout << "No es un digito, por favor intente de nuevo" << endl;
     return 1;
 }
-
-    
-    cout << "Ingrese el primer literal del coeficiente: ";
+    cout << "Ingrese el primer literal del coeficiente y su exponente (por ejemplo : x 2): ";
     cin >> exp.literal1>>exp.exponente_literal1;
     
     cout << "\nIngrese el segundo coeficiente: ";
@@ -111,18 +137,60 @@ int main() {
     cout << "No es un digito, por favor intente de nuevo" << endl;
     return 1;
 }
-    cout << "Ingrese el segundo literal del coeficiente: ";
+    cout << "Ingrese el segundo literal del coeficiente y su exponente (por ejemplo: y 2): ";
     cin >> exp.literal2>>exp.exponente_literal2; 
 
     cout << "(" << coeficiente1_ << exp.literal1 <<"^"<<exp.exponente_literal1<<" + " << coeficiente2_ << exp.literal2<<"^"<<exp.exponente_literal2<< ")^" << exponente<< endl;
-    
+
+    // Llamada a la función para mostrar el binomio expandido
     expansion_bionomio(exponente, coeficiente1_, coeficiente2_, exp.literal1, exp.exponente_literal2, exp.exponente_literal1, exp.exponente_literal2);
     // Llamada a la función para calcular y mostrar el resultado
     calcularResultado(exponente, coeficiente1_, coeficiente2_, exp);
-
-
     cout << endl;
+        break;
 
+    case '2': 
+    cout << "Ingresa el exponente del binomio: ";
+    cin >> exponente;
+    if (cin.fail()) {
+        cout << "No es un numero. Intente de nuevo." << endl;
+        return 1;
+    }
+    cout << "\nIngrese el primer coeficiente: ";
+    cin >> coeficiente1_;
+    if (cin.fail()) {
+        cout << "No es un digito, por favor intente de nuevo" << endl;
+        return 1;
+    }
     
-    return 0;
+    cout << "Ingrese el primer literal del coeficiente y su exponente (por ejemplo: x 2): ";
+    cin >> exp.literal1 >> exp.exponente_literal1;
+    
+    cout << "\nIngrese el segundo coeficiente: ";
+    cin >> coeficiente2_;
+    if (cin.fail()) {
+        cout << "No es un digito, por favor intente de nuevo" << endl;
+        return 1;
+    }
+
+    cout << "Ingrese el segundo literal del coeficiente y su exponente (por ejemplo: y 2): ";
+    cin >> exp.literal2 >> exp.exponente_literal2; 
+
+    //Llamada para ver la operacion
+    sin_expansion(exponente, coeficiente1_, coeficiente2_, exp.literal1, exp.literal2, exp.exponente_literal1, exp.exponente_literal2, num_aecontrar);
+    
+
+    cout << "Ingrese el digito que desea encontrar (para encontrar un numero se toma el 0 como 1 y el 1 como 2 asi sucesivamente): ";
+    cin >> num_aecontrar;
+   // Para mostrar como se hara la operacion
+    unsigned long long restaresultado = resta(exponente,num_aecontrar);
+    cout <<exponente<<"C"<<num_aecontrar<<" "<<"(" << coeficiente1_ << exp.literal1 <<"^"<<exp.exponente_literal1<<")"<<restaresultado<<"  " << coeficiente2_ << exp.literal2<<"^"<<exp.exponente_literal2<< ")^" << num_aecontrar<< endl;
+
+    // Llamada a la función para calcular y mostrar el resultado
+    resultado_sin(exponente, coeficiente1_, coeficiente2_, num_aecontrar, exp);
+    break;
+}
+
+
+return 0;
 }
